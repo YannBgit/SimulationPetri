e25876f1ffd83d7e52547febe00034f74ecf1a77
MelvinCRNR <creniermelvin@gmail.com>
2021-04-17 21:06:29 +0200

    Ajout de commentaires.

--- InterfaceGraphique.hpp
+++ InterfaceGraphique.hpp
@@ -7,14 +7,11 @@
 #include <QObject> // QObject est la classe de base de tous les objets sous Qt. La méthode connect() nous sera particulièrement utile pour les signaux et les slots
 #include <QtWidgets> // QtWidgets est un module qui nous permettera d'afficher nos fenêtres
 #include <QPushButton> // QPushButton nous permet de créer des objets QPushButton étant tout simplement des boutons
-#include <QRadioButton>
-/* QRadioButton nous permettera de créer des boutons radio. Les boutons radio nous seront utile dans la création du menu car ils permetteront de présenter sous la
-forme d'un QCM, des choix à l'utilisateur de sélectionner ce qu'il désire faire.
-*/
 #include <QLabel> // QLabel est utilisé pour afficher du texte ou une image. Cette classe nous sera utile pour l'affichage de l'échéancier.
 #include <QLayout> // QLayout nous permet de postionner nos widgets, comme par exemple, nos boutons.
 #include <QGroupBox> //QGroupBox fournit une "box" correspondant à un groupe avec un titre.
 #include "Moteur.hpp" // Pour utiliser la classe Moteur
+#include "Echancier.hpp" // Pour utiliser la classe Echancier
 #include "GestionnaireDeFichiers.hpp" // Pour utiliser la classe GestionnaireDeFichiers
 
 // CLASSE
