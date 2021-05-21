0777e7be5186c60cdf7ebc04c6d4e3688c0b0071
FaoWolf <48082281+FaoWolf@users.noreply.github.com>
2021-05-21 15:05:45 +0200

    Update Echeancier.hpp

--- Echeancier.hpp
+++ Echeancier.hpp
@@ -16,15 +16,12 @@ class Echeancier
     private:
     // VARIABLES
     int TempsCourant;
-    FILE *fichier;
-	FILE *temp;
 
     public:
     // CONSTRUCTEURS
     Echeancier(GestionnaireDeFichiers GDF)
     {
-        this->fichier = GDF.getFichier();
-	    this->temp = GDF.getTemp();
+
     }
 
     // DESTRUCTEUR
@@ -36,13 +33,13 @@ class Echeancier
     Argument de type Moteur pour trouver les informations à enregistrer.
     La fonction enregistre un état du réseau de Petri.
     */
-    void AjouterEtatReseau(Moteur M)
+    void AjouterEtatReseau(Moteur M, GestionnaireDeFichiers GDF)
     {
 	    StockerTempsCourant(M);
-	    GDF.EcrireEtat(M, this->temp);
+	    GDF.EcrireEtat(M, GDF.getTemp());
 
         return;
-	}
+    }
 
     /*
     Fonction de type Moteur pour renvoyer un état du réseau selon le temps courant.
@@ -52,7 +49,6 @@ class Echeancier
     Moteur RenvoyerEtatReseauCourant()
     {
 	    Moteur M = RenvoyerEtatReseauSelonTemps(TempsCourant);
-
 	    return M;
     }
 
@@ -66,7 +62,7 @@ class Echeancier
 	    this->TempsCourant = M.getTemps();
         
         return;
-	}
+    }
 
     /*
     Fonction de type Moteur pour renvoyer un état du réseau selon le temps cherché s'il existe ou le dernier temps stocké s'il n'existe
@@ -74,14 +70,14 @@ class Echeancier
     Argument de type int Te, le temps auquel on veut récupérer le réseau de Petri.
     La fonction recherche un état du réseau enregistré avec un temps donné et le renvoie.
     */
-    Moteur RenvoyerEtatReseauSelonTemps(int Te)
+    Moteur RenvoyerEtatReseauSelonTemps(int Te, GestionnaireDeFichiers GDF)
     {
-        char tableau[TAILLE_MAX];
-        char ligne[TAILLE_MAX];
-
-        char *chaine = "Te = ";
-        char *te = (char)Te;
-        char search[8];
+        Moteur M;
+	    
+        const char *chaine = "Te = ";
+        char te[3];
+	atol(Te,te,10);	// Conversion int en char
+        char search[9];
 
         /* Concaténation de la chaine sous la forme de "Te = [Te en paramètre]" */
         int i, j;
@@ -93,29 +89,21 @@ class Echeancier
         }
 
         // Concaténer te dans search   
-        for (j = 0; te[j]!='\0'; j++, i++)
+        for (j = 0; te[j] != '\0'; j++, i++)
         {
             search[i] = te[j];
         }
 
         search[i] = '\0';
-        int max = i;	// Nombre de caractère dans la chaine search
+ //       int max = i;	// Nombre de caractère dans la chaine search
         /* Fin de la concaténation */
 
         /* Recherche dans le fichier */
-        while(fgets(ligne, TAILLE_MAX, fichier) != NULL)
-        {
-            for(i = 0; i != max; i++)
-            {
-                if(search[i] != ligne[i])
-                {
-                    ligne += 1;
-                    i = 0;
-                }
-            }
-        }
+	M = GDF.rechercheChaine(search, GDF->temp);
+	/* Fin de la recherche dans le fichier et le stockage du moteur */
 
-        // return ;
+	free(search);
+	return M;
     }
 };
 
