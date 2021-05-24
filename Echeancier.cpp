3d7c0277441abcf8c800aa80fe90d2c4bfce4346
FaoWolf <48082281+FaoWolf@users.noreply.github.com>
2021-05-24 19:42:44 +0200

    Update Echeancier.cpp

--- Echeancier.cpp
+++ Echeancier.cpp
@@ -36,35 +36,10 @@ void Echeancier::StockerTempsCourant(Moteur M)
 Moteur Echeancier::RenvoyerEtatReseauSelonTemps(int Te, GestionnaireDeFichiers GDF)
 {
     Moteur M;
-    
-    const char *chaine = "Te = ";
-    char te[3];
-    atol(Te,te,10);	// Conversion int en char
-    char search[9];
-
-    /* Concaténation de la chaine sous la forme de "Te = [Te en paramètre]" */
-    int i, j;
-
-    // Pour itérer la première chaîne du début à la fin
-    for (i = 0; chaine[i] != '\0'; i++)
-    {
-        search[i] = chaine[i];
-    }
-
-    // Concaténer te dans search   
-    for (j = 0; te[j] != '\0'; j++, i++)
-    {
-        search[i] = te[j];
-    }
-
-    search[i] = '\0';
-    //       int max = i;	// Nombre de caractère dans la chaine search
-    /* Fin de la concaténation */
 
     /* Recherche dans le fichier */
-    M = GDF.rechercheChaine(search, GDF->temp);
+    M = GDF.rechercheChaine(search, GDF.getTemp);
     /* Fin de la recherche dans le fichier et le stockage du moteur */
 
-    free(search);
     return M;
 }
